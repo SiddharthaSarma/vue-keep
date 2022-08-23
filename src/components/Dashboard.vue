881dce1dd0a716010421681cee3e7f86ce3207be
unknown <sidduinevitable@gmail.com>
2022-08-23 21:59:55 +0530

    convert the sidebar sidebar static html to vue data

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -61,26 +61,10 @@
         <div class="flex flex-col justify-between mt-6">
           <aside>
             <ul>
-              <li>
-                <a class="flex items-center px-4 py-2 text-gray-700 hover:bg-yellow-100 rounded-lg" href="#">
-                  <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24"
-                    stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                      d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
-                  </svg>
-                  <span class="mx-4 font-medium">Dashboard</span>
-                </a>
-              </li>
-              <li>
-                <a class="flex items-center px-4 py-2 mt-5 text-gray-600 rounded-lg hover:bg-yellow-200" href="#">
-                  <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24"
-                    stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                      d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                      d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                  </svg>
-                  <span class="mx-4 font-medium">Settings</span>
+              <li v-for="item of sidebarList" :key="item.title">
+                <a class="flex items-center px-4 py-2 text-gray-700 hover:bg-yellow-200 rounded-lg" href="#">
+                  <component :is="item.icon" />
+                  <span class="mx-4 font-medium">{{item.title}}</span>
                 </a>
               </li>
             </ul>
@@ -89,7 +73,7 @@
       </div>
       <div class="w-full h-full p-4 m-8 overflow-y-auto">
         <div class="flex items-center justify-center p-16 mr-8 border-4 border-dotted lg:p-40">
-          Content...
+          {{content}}
         </div>
       </div>
     </div>
@@ -97,13 +81,22 @@
 </template>
 
 <script>
-import { ListIcon } from './icons';
+// eslint-disable-next-line
+import { DashboardIcon, ListIcon, SettingsIcon } from './icons';
 export default {
   name: "DashboardScreen",
-  components: { ListIcon },
+// eslint-disable-next-line
+  components: { ListIcon, DashboardIcon, SettingsIcon },
   data() {
     return {
-
+      content: 'content goes here',
+      sidebarList: [{
+        icon: DashboardIcon,
+        title: 'Dashboard'
+      }, {
+        icon: SettingsIcon,
+        title: 'Settings'
+      }]
     }
   }
 }
--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -1,1 +1,4 @@
+export { default as DashboardIcon } from './DashboardIcon.vue';
 export { default as ListIcon } from './List.vue';
+export { default as SettingsIcon } from './SettingsIcon.vue';
+
