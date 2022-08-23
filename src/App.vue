0955ee613dc4cd4dad09404b8d2420909666dcf4
unknown <sidduinevitable@gmail.com>
2022-08-23 09:30:25 +0530

    remove unnecessary code :wastebasket:

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,12 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 class="text-3xl">Vue keep</h1>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {},
+};
 </script>
