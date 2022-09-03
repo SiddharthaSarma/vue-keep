1054dd2ec5262fba9c47c47ef6c6a5e55a08225d
unknown <sidduinevitable@gmail.com>
2022-09-03 22:32:12 +0530

    update path for easy import

--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -1,8 +1,7 @@
-export { default as ArchiveIcon } from './ArchiveIcon.vue';
-export { default as BellIcon } from './BellIcon.vue';
-export { default as BulbIcon } from './BulbIcon.vue';
-export { default as ListIcon } from './List.vue';
-export { default as PencilIcon } from './PencilIcon.vue';
-export { default as SettingsIcon } from './SettingsIcon.vue';
-export { default as TrashIcon } from './TrashIcon.vue';
-
+export { default as ArchiveIcon } from './src/ArchiveIcon.vue';
+export { default as BellIcon } from './src/BellIcon.vue';
+export { default as BulbIcon } from './src/BulbIcon.vue';
+export { default as ListIcon } from './src/List.vue';
+export { default as PencilIcon } from './src/PencilIcon.vue';
+export { default as SettingsIcon } from './src/SettingsIcon.vue';
+export { default as TrashIcon } from './src/TrashIcon.vue';
