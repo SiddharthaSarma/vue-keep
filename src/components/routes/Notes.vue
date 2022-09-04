35d280b9ae801fc67507f5a914a9dfc1dbbf196c
unknown <sidduinevitable@gmail.com>
2022-09-04 22:37:00 +0530

    cosmetic changes for take a note

--- src/components/routes/Notes.vue
+++ src/components/routes/Notes.vue
@@ -1,15 +1,21 @@
 <template>
-  <div class="w-1/2">
+  <div class="w-1/2 border shadow-md text-2xl text-gray-900 sm:text-sm rounded-lg focus:outline-none focus:ring-1 flex flex-col">
     <input
       type="text"
       name="name"
-      class="border shadow-md text-2xl text-gray-900 sm:text-sm rounded-lg focus:outline-none focus:ring-1 block w-full pl-10 p-2.5"
-      placeholder="Search"
+      class="text-2xl text-gray-900 sm:text-sm rounded-lg focus:outline-none focus:ring-1 block w-full p-2.5"
+      :placeholder="!opened ? 'Take a note...' : 'Title'"
+      @click="opened = true"
     />
+    <textarea v-if="opened" class="mt-4" placeholder="Take a note"></textarea>
+    <div contenteditable="true">Take a  note</div>
   </div>
 </template>
 <script>
 export default {
   name: "NotesScreen",
+  data: () => ({
+    opened: false
+  })
 };
 </script>
