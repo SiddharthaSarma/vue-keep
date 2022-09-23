9b719eb40ad092e6d1a544ecdb729af8f732c8bb
unknown <sidduinevitable@gmail.com>
2022-09-23 19:44:50 +0530

    clean right side of the top bar

---
+++ src/components/dashboard/RightDashboard.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="flex items-center justify-center bg-blue-100 rounded-full w-10 h-10">M</div>
+</template>
+<script>
+export default {
+  name: 'RightDashboard',
+};
+</script>
