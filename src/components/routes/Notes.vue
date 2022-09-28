24e0349186fed8f4b74353c85fec9072aa5e3717
unknown <sidduinevitable@gmail.com>
2022-09-28 16:06:43 +0530

    fix cosmetic issue in notes section

--- src/components/routes/Notes.vue
+++ src/components/routes/Notes.vue
@@ -1,74 +1,76 @@
 <template>
-  <div class="w-5/12 border shadow-md text-gray-900 rounded-lg flex flex-col">
-    <div class="flex justify-center pr-2 h-12 text-2xl">
-      <input
-        v-model="title"
-        type="text"
-        name="title"
-        class="text-sm text-gray-900 rounded-lg focus:outline-none block w-full p-2.5"
-        :placeholder="!opened ? 'Take a note...' : 'Title'"
-        @focusin="handleTitleFocusIn"
-        @focusout="handleTitleFocusOut"
-        @click="opened = true"
+  <div class="w-full">
+    <div class="w-5/12 border shadow-md text-gray-900 rounded-lg flex flex-col m-auto">
+      <div class="flex justify-center pr-2 h-12 text-2xl">
+        <input
+          v-model="title"
+          type="text"
+          name="title"
+          class="text-sm text-gray-900 rounded-lg focus:outline-none block w-full p-2.5"
+          :placeholder="!opened ? 'Take a note...' : 'Title'"
+          @focusin="handleTitleFocusIn"
+          @focusout="handleTitleFocusOut"
+          @click="opened = true"
+        />
+        <div class="flex items-center h-full">
+          <template v-if="!opened">
+            <div class="icon-top" @click="handleCheckListClick">
+              <CheckSquareIcon />
+            </div>
+            <div class="icon-top">
+              <BrushIcon />
+            </div>
+            <div class="icon-top">
+              <ImageIcon />
+            </div>
+          </template>
+          <template v-if="opened">
+            <div class="icon-top">
+              <PinIcon />
+            </div>
+          </template>
+        </div>
+        <div class="flex items-center h-full" v-if="opened"></div>
+      </div>
+      <p
+        v-if="opened"
+        contenteditable="true"
+        class="focus-visible:outline-none p-2.5 text-sm"
+        @input="handleInput"
+        @focusin="handleContentFocusIn"
+        @focusout="handleContentFocusOut"
+        v-html="content"
       />
-      <div class="flex items-center h-full">
-        <template v-if="!opened">
-          <div class="icon-top" @click="handleCheckListClick">
-            <CheckSquareIcon />
+      <div v-if="opened" class="flex justify-between items-center p-2">
+        <div class="left flex justify-between w-1/2">
+          <div class="icon">
+            <BellIcon />
           </div>
-          <div class="icon-top">
-            <BrushIcon />
+          <div class="icon">
+            <UserPlusIcon />
           </div>
-          <div class="icon-top">
+          <div class="icon">
+            <PaletteIcon />
+          </div>
+          <div class="icon">
             <ImageIcon />
           </div>
-        </template>
-        <template v-if="opened">
-          <div class="icon-top">
-            <PinIcon />
+          <div class="icon">
+            <FileZipIcon />
           </div>
-        </template>
-      </div>
-      <div class="flex items-center h-full" v-if="opened"></div>
-    </div>
-    <p
-      v-if="opened"
-      contenteditable="true"
-      class="focus-visible:outline-none p-2.5 text-sm"
-      @input="handleInput"
-      @focusin="handleContentFocusIn"
-      @focusout="handleContentFocusOut"
-      v-html="content"
-    />
-    <div v-if="opened" class="flex justify-between items-center p-2">
-      <div class="left flex justify-between w-1/2">
-        <div class="icon">
-          <BellIcon />
+          <div class="icon">
+            <ThreeDotsVertical />
+          </div>
+          <div class="icon" :class="[disableEditing ? 'disabled' : null]">
+            <ArrowCounterClockWise />
+          </div>
+          <div class="icon" :class="[disableEditing ? 'disabled' : null]">
+            <ArrowClockWise />
+          </div>
         </div>
-        <div class="icon">
-          <UserPlusIcon />
+        <div class="right">
+          <button class="btn">Close</button>
         </div>
-        <div class="icon">
-          <PaletteIcon />
-        </div>
-        <div class="icon">
-          <ImageIcon />
-        </div>
-        <div class="icon">
-          <FileZipIcon />
-        </div>
-        <div class="icon">
-          <ThreeDotsVertical />
-        </div>
-        <div class="icon" :class="[disableEditing ? 'disabled' : null]">
-          <ArrowCounterClockWise />
-        </div>
-        <div class="icon" :class="[disableEditing ? 'disabled' : null]">
-          <ArrowClockWise />
-        </div>
-      </div>
-      <div class="right">
-        <button class="btn">Close</button>
       </div>
     </div>
   </div>
