70ae5813ba02f72dcf5b30405a1cbc55849662be
unknown <sidduinevitable@gmail.com>
2022-09-30 16:33:02 +0530

    complete archive screen as per original screen

--- src/components/routes/Archive.vue
+++ src/components/routes/Archive.vue
@@ -1,8 +1,19 @@
 <template>
-  <div>Archive</div>
+  <div class="flex flex-col h-full">
+    <div class="flex flex-col items-center grow justify-center">
+      <ArchiveIcon width="120" height="120" color="#CCCCCC" />
+      <div class="content text-gray-500 text-xl">
+        Your archived notes appear here
+      </div>
+    </div>
+  </div>
 </template>
 <script>
+import {ArchiveIcon} from '../icons'
 export default {
   name: 'ArchiveScreen',
+  components: {
+   ArchiveIcon 
+  }
 };
 </script>
--- src/components/routes/TrashScreen.vue
+++ src/components/routes/TrashScreen.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="flex flex-col h-full">
-    <div class="heading h-16 italic text-lg">Notes in Trash are deleted after 7 days.</div>
+    <div class="heading h-16 italic text-lg">
+      Notes in Trash are deleted after 7 days.
+    </div>
     <div class="flex flex-col items-center grow justify-center">
       <TrashIcon width="120" height="120" color="#CCCCCC" />
       <div class="content text-gray-500 text-xl">No notes in Trash</div>
